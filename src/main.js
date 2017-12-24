54b63001afadab56a3746672c80b91661c1ddaca
pengfei.yuan <pengfei.yuan@boldseas.com>
2017-12-24 20:34:04 +0800

    rainy mood

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App'
 import './styles/style.css';
+import './styles/antd.css';
+import './styles/font/iconfont.css';
 
 let rootElement = document.getElementById('root');
 if (!rootElement) rootElement = document.createElement('div');
